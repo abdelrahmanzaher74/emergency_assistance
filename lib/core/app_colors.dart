8a177867bf52fe8ceb0d40f4f56d3f3730d26b62
Abdelrahman Zaher <abdelrahmanzaher143@gmail.com>
2026-07-01 11:09:34 +0300

    core files for design

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xffF5F7FA);
+
+  static const police = Color(0xff2563EB);
+
+  static const ambulance = Color(0xff10B981);
+
+  static const fire = Color(0xffEF4444);
+
+  static const road = Color(0xffF59E0B);
+
+  static const text = Color(0xff1E293B);
+}
